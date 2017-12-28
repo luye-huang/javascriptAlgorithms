dfce98525b32ee75a8d677e8bff406346a0c845c
huangluye <huangluye@qiyi.com>
2017-12-28 11:06:40 +0800

    111

--- tree.js
+++ tree.js
@@ -76,44 +76,70 @@
 // 103
 
 // Definition for a binary tree node.
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
+// function TreeNode(val) {
+//     this.val = val;
+//     this.left = this.right = null;
+// }
 /*
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var zigzagLevelOrder = function (root) {
-    if(!root)return [];
-    const queue = [], res = [];
-    let order = false, layer = 0, row = [];
-    root.layer = layer;
-    queue.push(root);
-    while (queue.length > 0) {
-        const node = queue.pop();
-        if (node.layer == layer) {
-            row.push(node.val);
-            if (node.left) {
-                node.left.layer = layer + 1;
-                queue.unshift(node.left);
-            }
-            if (node.right) {
-                node.right.layer = layer + 1;
-                queue.unshift(node.right);
-            }
-        } else {
-            queue.push(node);
-            layer++;
-            order && row.reverse();
-            res.push(row);
-            row = [];
-            order = !order;
-        }
+// var zigzagLevelOrder = function (root) {
+//     if(!root)return [];
+//     const queue = [], res = [];
+//     let order = false, layer = 0, row = [];
+//     root.layer = layer;
+//     queue.push(root);
+//     while (queue.length > 0) {
+//         const node = queue.pop();
+//         if (node.layer == layer) {
+//             row.push(node.val);
+//             if (node.left) {
+//                 node.left.layer = layer + 1;
+//                 queue.unshift(node.left);
+//             }
+//             if (node.right) {
+//                 node.right.layer = layer + 1;
+//                 queue.unshift(node.right);
+//             }
+//         } else {
+//             queue.push(node);
+//             layer++;
+//             order && row.reverse();
+//             res.push(row);
+//             row = [];
+//             order = !order;
+//         }
+//     }
+//     order && row.reverse();
+//     res.push(row);
+//     return res;
+// };
+//
+
+
+// 111
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+    const level = arguments[1] ? arguments[1] : 0;
+    if (!root) return level;
+    if (!root.left && !root.right) {
+        return level + 1;
+    } else if (root.left && !root.right) {
+        return arguments.callee(root.left, level + 1);
+    } else if (!root.left && root.right) {
+        return arguments.callee(root.right, level + 1);
+    } else {
+        return Math.min(arguments.callee(root.left, level + 1), arguments.callee(root.right, level + 1));
     }
-    order && row.reverse();
-    res.push(row);
-    return res;
 };
 
 const node1 = new TreeNode(3);
@@ -123,7 +149,7 @@ const node4 = new TreeNode(15);
 const node5 = new TreeNode(7);
 node1.left = node2;
 node1.right = node3;
-node3.left = node4;
+node2.left = node4;
 node3.right = node5;
 //
-console.log(zigzagLevelOrder(null));
+console.log(minDepth(node1));
