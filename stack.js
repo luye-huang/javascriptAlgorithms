6cfca35940a14de3fcefe41bf4a5b9ea4f7f91b2
huangluye <huangluye@qiyi.com>
2019-04-08 12:42:11 +0800

    316 done

--- stack.js
+++ stack.js
@@ -1,6 +1,70 @@
 /**
  * Created by luye on 28/03/2019.
  */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicateLetters = function (s) {
+    const len = s.length, times = {}, visited = {}, stack = new Stack();
+    for (let i = 0; i < len; i++) {
+        const c = s.charAt(i);
+        if (times[c]) {
+            times[c]++;
+        } else {
+            times[c] = 1;
+        }
+    }
+    for (let i = 0; i < len; i++) {
+        const c = s.charAt(i);
+        times[c]--;
+        if (visited[c]) {
+            continue;
+        }
+        while (stack.size() && stack.peek() > c && times[stack.peek()]) {
+            const dump = stack.pop();
+            visited[dump] = false;
+        }
+        stack.push(c);
+        visited[c] = true;
+    }
+    return stack.stringfy();
+}
+
+class Stack{
+    constructor(value){
+        this.list = value || [];
+    }
+
+    peek() {
+        return this.list[this.size() - 1];
+    }
+
+    pop() {
+        return this.list.pop();
+    }
+
+    push(el) {
+        this.list.push(el);
+    }
+
+    size() {
+        return this.list.length;
+    }
+
+    stringfy(){
+        let res = '';
+        this.list.forEach(el => {
+            res += el;
+        });
+        return res;
+    }
+}
+
+console.log(removeDuplicateLetters("bcabc"));
+console.log(removeDuplicateLetters("cbacdcbc"));
+
 /**
  * 735
  * @param {number[]} asteroids
